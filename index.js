f30fb8ba17df1ce15620b8b73afdcd0a190f9357
Elena Chernikova <lena@acvos.com>
2022-02-18 16:49:55 -0800

    fetch by coordinates function added

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const request = require("request");
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
+  fetchCoordsByIP(ip, (coords, error) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
 
-  console.log('It worked! Returned IP:' , ip);
+    console.log('It worked!');
+    console.log('Returned IP:', ip);
+    console.log('Returned coordinates:', coords);
+  });
 });
+
+
+// const fetchMyIP = function(callback) {
+//   request('https://api.ipify.org?format=json', (error, response, body) => {
+
+// // error can be set if invalid domain, user is offline, etc.
+//     if (error) {
+//       callback(error, null);
+//     return;
+//     }
+// // if non-200 status, assume server error
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//     callback(Error(msg), null);
+//     return;
+//     }
+
+//     const ip = JSON.parse(body).ip;
+//     callback(null, ip);
+//   });
+// };
--- iss.js
+++ iss.js
@@ -1,4 +1,5 @@
 const request = require('request');
+// const requestPromise = require('request-promise');
 
 /**
  * Makes a single API request to retrieve the user's IP address.
@@ -8,12 +9,20 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+
+
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
 
+// error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+// if non-200 status, assume server error
     if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(msg, null);
       return;
     }
 
@@ -22,4 +31,31 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+/* Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
+
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/174.4.198.194`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
